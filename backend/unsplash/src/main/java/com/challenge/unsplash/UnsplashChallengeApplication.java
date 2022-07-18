f1c4496ec37b98a8e22092925d1b88e7f9378759
VitorFirminoSilva <vitorfirmino70@gmail.com>
2022-07-18 12:52:23 -0300

    INITIAL COMMIT

---
+++ backend/unsplash/src/main/java/com/challenge/unsplash/UnsplashChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.unsplash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsplashChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsplashChallengeApplication.class, args);
+	}
+
+}
---
+++ backend/unsplash/src/test/java/com/challenge/unsplash/UnsplashChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.unsplash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsplashChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
